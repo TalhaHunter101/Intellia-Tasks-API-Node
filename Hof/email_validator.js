d172fbef553ce7db7b4d2069952c52ce72cee836
TalhaHunter101 <talha.shafeeq.ts@gmail.com>
2022-06-29 12:29:44 +0500

    Finalize

---
+++ Hof/email_validator.js
@@ -0,0 +1,10 @@
+function validateEmailAddress(input) {
+    var regex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (regex.test(input)) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+module.exports = { validateEmailAddress };
--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 
 const _ = require("lodash");
 const { v4: uuid } = require("uuid");
-
+const emailValidator = require("../Hof/email_validator");
 
 //Adding User Data
 exports.adduser = async (req, res) => {
@@ -12,8 +12,20 @@ exports.adduser = async (req, res) => {
     const content = req.body;
 
     if (!content) {
-        return res.sendStatus(400);
+        return res.status(400).json({
+            message: "No content sent by request"
+        });
+    } else if (content.UserName == "" || content.UserName == undefined) {
+        res.status(400).json({ Message: "Missing User Name:: Try again" });
+        return;
+    } else if (content.UserEmail == "" || content.UserEmail == undefined) {
+        res.status(400).json({ Message: "Missing Email:: Try again" });
+        return;
+    } else if (emailValidator.validateEmailAddress(content.UserEmail) === -1) {
+        res.status(400).json({ Message: "Incorrect email :: Enter again" });
+        return;
     }
+    // I have assumed other fields as optional
 
     await fs.mkdir("data/userdata", { recursive: true });
     await fs.writeFile(`data/userdata/${id}.json`, JSON.stringify(content));
@@ -29,9 +41,34 @@ exports.adduser = async (req, res) => {
 // Adding Job Experience
 exports.addExperience = async (req, res) => {
     const id = req.params.id;
-    console.log(id);
-    const Experience = req.body;
-    console.log(Experience)
+    // console.log(id);
+    let Experience = req.body;
+    // console.log(Experience)
+    if (!Experience) {
+        return res.status(400).json({
+            message: "No Experience sent by request"
+        });
+    } else if (Experience.JobTitle == "" || Experience.JobTitle == undefined) {
+        res.status(400).json({ Message: "Missing Job Title:: Try again" });
+        return;
+    } else if (Experience.Company == "" || Experience.Company == undefined) {
+        res.status(400).json({ Message: "Missing Company:: Try again" });
+        return;
+    } else if (Experience.Country == "" || Experience.Country == undefined) {
+        res.status(400).json({ Message: "Missing Country:: Try again" });
+        return;
+    } else if (Experience.City == "" || Experience.City == undefined) {
+        res.status(400).json({ Message: "Missing City:: Try again" });
+        return;
+    } else if (Experience.Status == "" || Experience.Status == undefined) {
+        res.status(400).json({ Message: "Missing Status:: Try again" });
+        return;
+    } else if (Experience.Fromdate == "" || Experience.Fromdate == undefined) {
+        res.status(400).json({ Message: "Missing Fromdate:: Try again" });
+        return;
+    } if (Experience.Status == 'Current') {
+        Experience.Todate = "";
+    }
 
     // let content = await fs.readFile(`data/userdata/${id}.json`, 'utf-8');
     let content = require(`../data/userdata/${id}.json`);
@@ -51,21 +88,44 @@ exports.addExperience = async (req, res) => {
 // Adding Education 
 exports.addEducation = async (req, res) => {
     const id = req.params.id;
-    console.log(id);
+    // console.log(id);
     const Education = req.body;
-    console.log(Education)
+    // console.log(Education)
+    if (!Education) {
+        return res.status(400).json({
+            message: "No Education sent by request"
+        });
+    } else if (Education.UniversitySchool == "" || Education.UniversitySchool == undefined) {
+        res.status(400).json({ Message: "Missing University School name:: Try again" });
+        return;
+    } else if (Education.Qualification == "" || Education.Qualification == undefined) {
+        res.status(400).json({ Message: "Missing Qualification:: Try again" });
+        return;
+    } else if (Education.AreaofStudy == "" || Education.AreaofStudy == undefined) {
+        res.status(400).json({ Message: "Missing Area of Study:: Try again" });
+        return;
+    } else if (Education.GradeGPA == "" || Education.GradeGPA == undefined) {
+        res.status(400).json({ Message: "Missing Grade / GPA:: Try again" });
+        return;
+    } else if (Education.FromDate == "" || Education.FromDate == undefined) {
+        res.status(400).json({ Message: "Missing From Date:: Try again" });
+        return;
+    } else if (Education.ExpectedDate == "" || Education.ExpectedDate == undefined) {
+        res.status(400).json({ Message: "Missing Expected Date:: Try again" });
+        return;
+    } else {
+        // let content = await fs.readFile(`data/userdata/${id}.json`, 'utf-8');
+        let content = require(`../data/userdata/${id}.json`);
+        // content.Education.push(Education);
+        content.Education.push(Education);
 
-    // let content = await fs.readFile(`data/userdata/${id}.json`, 'utf-8');
-    let content = require(`../data/userdata/${id}.json`);
-    // content.Education.push(Education);
-    content.Education.push(Education);
+        await fs.writeFile(`data/userdata/${id}.json`, JSON.stringify(content));
 
-    await fs.writeFile(`data/userdata/${id}.json`, JSON.stringify(content));
-
-    res.status(201).json({
-        message: `Education Added to this ${id} person.`,
-        data: Education
-    });
+        res.status(201).json({
+            message: `Education Added to this ${id} person.`,
+            data: Education
+        });
+    }
 };
 
 //Getting User Data
